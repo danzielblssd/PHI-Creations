d54409262dc3beafce1ec1783934fcf5aa471ad0
Danziel Scruse <73694182+danzielblssd@users.noreply.github.com>
2021-05-31 21:46:27 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -30,4 +30,26 @@ menuBtn.addEventListener("click", () => {
   }
 });
 TweenLite.to(myText, 5, { x: 600, ease: new SlowMo(0.5, 0.8) });
-// ------------------------------------------------
+// ------------------Contact--------------------------
+// var form = document.getElementById("my-form");
+
+// async function handleSubmit(event) {
+//   event.preventDefault();
+//   var status = document.getElementById("my-form-status");
+//   var data = new FormData(event.target);
+//   fetch(event.target.action, {
+//     method: form.method,
+//     body: data,
+//     headers: {
+//       Accept: "application/json",
+//     },
+//   })
+//     .then((response) => {
+//       status.innerHTML = "Thanks for your submission!";
+//       form.reset();
+//     })
+//     .catch((error) => {
+//       status.innerHTML = "Oops! There was a problem submitting your form";
+//     });
+// }
+// form.addEventListener("submit", handleSubmit);
